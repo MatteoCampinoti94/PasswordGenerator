95d41e33b8b0f4c82a24af6db69ce75be70b0f65
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2018-04-17 17:01:31 +0200

    Added options for extra randomization

--- pwdgen.py
+++ pwdgen.py
@@ -1,5 +1,6 @@
 import os, sys
-import secrets
+import secrets, random
+import string
 from itertools import groupby
 from operator import itemgetter
 
@@ -9,6 +10,8 @@ if not os.path.isfile('nouns.txt') or not os.path.isfile('adjectives.txt'):
 length_min = 8
 length_max = 0
 total = 5
+rand_ord = False
+rand_sym = False
 
 if len(sys.argv) > 1:
     i = 0
@@ -39,6 +42,10 @@ if len(sys.argv) > 1:
                 length_min = 8
                 length_max = 0
             i += 1
+        elif sys.argv[i] == '-r':
+            rand_ord = True
+        elif sys.argv[i] == '-R':
+            rand_sym = True
         i += 1
 
 if (length_max != 0 and length_max < length_min) or length_min < 0:
@@ -58,21 +65,25 @@ with open("adjectives.txt", "r") as f:
 pwds = []
 i = 0
 while len(pwds) < total and i < 100000:
-    r = secrets.randbelow(len(nouns))
-    n = nouns[r]
+    rn = secrets.randbelow(len(nouns))
+    n = nouns[rn]
 
-    r = secrets.randbelow(len(adjct))
-    a = list(adjct.keys())[r]
+    ra = secrets.randbelow(len(adjct))
+    a1 = adjct[list(adjct.keys())[ra]]
+    a1 = a1[secrets.randbelow(len(a1))]
 
-    r1 = secrets.randbelow(len(adjct[a]))
-    while True:
-        r2 = secrets.randbelow(len(adjct[a]))
-        if r2 != r1: break
-
-    if r1 < r2:
-        pwd = f'{adjct[a][r1]} {adjct[a][r2]} {n}'.title()
+    if not rand_ord:
+        a2 = adjct[list(adjct.keys())[ra]]
+        a2 = a2[secrets.randbelow(len(a2))]
     else:
-        pwd = f'{adjct[a][r2]} {adjct[a][r1]} {n}'.title()
+        ra = secrets.randbelow(len(adjct))
+        a2 = adjct[list(adjct.keys())[ra]]
+        a2 = a2[secrets.randbelow(len(a2))]
+
+    if a1 < a2:
+        pwd = f'{a1} {a2} {n}'.title()
+    else:
+        pwd = f'{a2} {a1} {n}'.title()
 
     if pwd not in pwds:
         if len(pwd.replace(' ', '')) >= length_min:
@@ -83,4 +94,19 @@ while len(pwds) < total and i < 100000:
     i += 1
 
 pwds.sort()
+
+if rand_sym:
+    symbols = list(string.digits + string.punctuation)
+    random.shuffle(symbols)
+
+    for i in range(0, len(pwds)):
+        pwd = list(pwds[i].replace(' ', '').lower())
+        for j in range(0, secrets.randbelow(len(pwd)//2)+1):
+            x = secrets.choice(range(0, len(pwd)))
+            pwd[j] = secrets.choice(symbols)
+        for j in range(0, secrets.randbelow(len(pwd)//2)+1):
+            x = secrets.choice(range(0, len(pwd)))
+            pwd[j] = pwd[j].upper()
+        pwds[i] = ''.join(pwd)
+
 print('\n'.join(pwds))
